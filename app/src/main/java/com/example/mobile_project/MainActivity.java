182837751a1275d53206a159891e91ffe627aa11
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2023-10-31 15:58:33 -0400

    Changes to the home and signup page, put comments to indicate my intentions

--- app/src/main/java/com/example/mobile_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_project/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.mobile_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginUser, createUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        loginUser = findViewById(R.id.loginButton);
+        createUser = findViewById(R.id.registerUser);
+
+        //If the user does not have an account, they can click this button to create on.
+        createUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSignUp = new Intent(MainActivity.this, SignUpPage.class);
+                startActivity(toSignUp);
+            }
+        });
+
+        //If the user has an account, they should be taken to a page with their profile
+
+
+    }//onCreate method
+}//class MainActivity
--- app/src/main/java/com/example/mobile_project/SignUpPage.java
+++ app/src/main/java/com/example/mobile_project/SignUpPage.java
@@ -2,13 +2,32 @@ package com.example.mobile_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpPage extends AppCompatActivity {
+    Button loginReturn, userRegistered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        loginReturn = findViewById(R.id.backToLogin);
+        userRegistered = findViewById(R.id.registerUserAccount);
+
+        loginReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnToLogin = new Intent(SignUpPage.this, MainActivity.class);
+                startActivity(returnToLogin);
+            }
+        });
+
+        //Once the user creates an account, they should be taken to a new page to edit their profile
+
+
     }
 }
